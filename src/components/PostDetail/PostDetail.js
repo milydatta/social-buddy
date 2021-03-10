f9d269e4a049a3b2f6e614687d4690cedb08483b
mily datta <dmily270@gmail.com>
2021-03-10 10:46:42 +0600

    some update

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import Comment from '../Comment/Comment';
+// import Comment from '../Comment/Comment';
 
 const PostDetail = () => {
     const {id} = useParams();
@@ -25,11 +25,11 @@ const PostDetail = () => {
             <h3>This is Post detail: {id}</h3>
             <p>User posted: {post.id}</p>
             <p>title: {post.title}</p>
-             <p>post body: {post.body}</p>
-             <p>Number of comments: {comments.length}</p>
-             {
-                 comments.map(comment => <Comment comment={comment}></Comment>)
-             }
+            <p>post body: {post.body}</p>
+            <p>Number Of Comments: {comments.length}</p>
+             {/* {
+                comments.map(comment => <Comment>{comments}</Comment>)
+             } */}
         </div>
     );
 };
